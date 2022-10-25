ad54a802dda927b0d57fa91fe0903ed1cb34fdeb
SADIA AFSANA <afsanasadia2022@gmail.com>
2022-10-25 22:09:16 +0600

    route setup and layout done

---
+++ src/Layout/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Shared/Footer/Footer';
+import Header from '../Shared/Header/Header';
+import LeftSideNav from '../Shared/LeftSideNav/LeftSideNav';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Container>
+                <Row>
+                    <Col lg="4">
+                        <LeftSideNav></LeftSideNav>
+                    </Col>
+                    <Col lg="8">
+                        <Outlet></Outlet>
+                    </Col>
+
+                </Row>
+            </Container>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Main;
